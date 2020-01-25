62b73c317229db7ab00f1459a6a3a193f0b4bf62
Jonathan Chan <johnkchan252@gmail.com>
2020-01-25 16:05:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let submit = document.querySelector("submit");
-  let description = document.querySelector("#new-task-description");
+  let description = document.querySelector("new-task-description");
   let tasks = document.querySelector("tasks")
   
   submit.addEventListener("click", (event) => {
