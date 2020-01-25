0d2d86a5d930e49be1f69d82485fc7f018e33ec0
Jonathan Chan <johnkchan252@gmail.com>
2020-01-25 16:05:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
   submit.addEventListener("click", (event) => {
     event.preventDefault();
     var task = description.value;
-    tasks.innerHTML += `<div id="list-${todo}"><li>${todo}<button>X</button></div>`;
+    tasks.innerHTML += `<div id="list-${task}"><li>${task}<button>X</button></div>`;
   })
 });
