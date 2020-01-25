2c832359cfd1020dfb30c963f6f9a2ba77a78ae1
Jonathan Chan <johnkchan252@gmail.com>
2020-01-25 15:59:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let form = document.querySelector("#create-task-form");
-  let description = document.querySelector("#create-task-form")
+  let description = document.querySelector("#new-task-description");
+  
 });
